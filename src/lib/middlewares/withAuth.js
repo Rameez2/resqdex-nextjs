de6a724c71718420a10b24f19106fb08a360c16b
Rameez <syedrameezshahpesh@gmail.com>
2025-04-03 19:14:25 +0500

    organization application Pending Message

--- src/lib/middlewares/withAuth.js
+++ src/lib/middlewares/withAuth.js
@@ -26,6 +26,16 @@ const withAuth = (WrappedComponent) => {
       return null; // Prevent rendering while redirecting
     }
 
+    if (user.role === "Organization" && user.status === "Pending") {
+      return (
+        <>
+          <h1 className="text-3xl font-semibold text-gray-800 mb-4 text-center">Application Under Review</h1>
+          <p className="text-lg text-gray-600 text-center">Your application is currently being reviewed. Please wait for admin approval.</p>
+        </>
+
+      )
+    }
+
     return <WrappedComponent {...props} />;
   };
 };
